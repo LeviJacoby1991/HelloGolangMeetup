3f34b461fec69fb56f32bcbf12b66964e3a3cab7
Levi <levi.jacoby@fathom5.co>
2023-02-15 09:02:00 -0600

    first commit

---
+++ HelloGolang.go
@@ -0,0 +1,12 @@
+package helloworld
+
+import (
+	"fmt"
+
+	"github.com/LeviJacoby1991/fizbuzz"
+)
+
+func hellowrld() {
+	fmt.Println("Hello Golang!")
+	fizbuzz.Fizzbuzz()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/LeviJacoby1991/HelloGolangMeetup
+
+go 1.19
+
+require github.com/LeviJacoby1991/fizbuzz v0.0.0-20230215004417-9deb75b0f6a8 // indirect
